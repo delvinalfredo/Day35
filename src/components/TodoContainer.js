20ab059fa6c8e4af7d31d724f7c6832de57c3578
delvinalfredo <delvinalfredo@gmail.com>
2022-04-17 17:48:40 +0700

    rfc

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -4,15 +4,30 @@ import AddTodo from './AddTodo'
 import TodoCard from './TodoCard'
 
 const TodoContainer = () => {
-    const [todoList, setTodoList] = useState([])
+    const [todoList, setTodoList] = useState([
+        {
+            index:0,
+            title: 'Learn React',
+            description: 'How to code reactJS',
+            priority: 'Medium',
+            note: 'no note'
+        }
+    ])
 
     const handleAddTodo = (newTodo) => {
         const tempNewTodo = {
             index: todoList.length + 1,
             title: newTodo.title,
-            description: newTodo.description
+            description: newTodo.description,
+            priority: newTodo.priority,
+            note: newTodo.note
         }
         setTodoList([...todoList, tempNewTodo])
+    }
+
+    const handleEditTodo = (editedTodo) => {
+        const newTodoList = todoList.splice(editedTodo.index, 1, editedTodo)
+        setTodoList(newTodoList)
     }
 
     const handleRemoveTodo = (index) => {
@@ -24,7 +39,7 @@ const TodoContainer = () => {
         <section>
             <AddTodo handleAddTodo={handleAddTodo} />
             {todoList.map((todo,index) => {
-                return <TodoCard  key={index} todo={todo} handleRemoveTodo={handleRemoveTodo} />
+                return <TodoCard  key={index} todo={todo} handleRemoveTodo={handleRemoveTodo} handleEditTodo={handleEditTodo} />
             })}
         </section>
     )
